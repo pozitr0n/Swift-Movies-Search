89d0c99ed3332c1e609a0dcb8350b3b4b6d51783
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-06-17 04:51:00 +0200

    Start working with MainViewController.swift, adding UISearchBar and UICollectionView

--- FilmsApp/Controller/MainViewController/MainViewController.swift
+++ FilmsApp/Controller/MainViewController/MainViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var moviesSearchBar: UISearchBar!
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        initializeDataSourceDelegates()
+        
+    }
+    
+    // Initialization of standard properties: dataSource and delegate
+    //
+    func initializeDataSourceDelegates() {
+        
+        mainCollectionView.dataSource = self
+        mainCollectionView.delegate = self
+        moviesSearchBar.delegate = self
+        
     }
 
+}
+
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
 }
 
