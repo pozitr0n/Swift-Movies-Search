b32e2d1f6e25a3fca3eb32aff41972637112e043
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-06-27 17:29:32 +0200

    Added new elements into MovieDetailsViewController and new DetailsPreviewCollectionViewCell class for the cell contains image (preview)

---
+++ FilmsApp/Controller/MovieDetailsViewController/DetailsPreviewCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DetailsCollectionViewCell.swift
+//  FilmsApp
+//
+//  Created by Raman Kozar on 27/06/2023.
+//
+
+import UIKit
+
+class DetailsPreviewCollectionViewCell: UICollectionViewCell {
+  
+    @IBOutlet weak var imageDetailsImageView: UIImageView!    
+    
+}
--- FilmsApp/Controller/MovieDetailsViewController/MovieDetailsViewController.swift
+++ FilmsApp/Controller/MovieDetailsViewController/MovieDetailsViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
 
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
+    @IBOutlet weak var releaseYearLabel: UILabel!
+    
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    @IBOutlet weak var imageGalleryCollection: UICollectionView!
+    
+    @IBOutlet weak var movieDescriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
