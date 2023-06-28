408ef714d5ad77d97a05e9c8fc2d01edf1d18444
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-06-28 02:28:51 +0200

    New changes with the interface (details view controller)

--- FilmsApp/Controller/MovieDetailsViewController/MovieDetailsViewController.swift
+++ FilmsApp/Controller/MovieDetailsViewController/MovieDetailsViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
 
+    // Setting the parameters
+    private let moviePosterImageViewCornerRadius: CGFloat = 10
+    
     @IBOutlet weak var moviePosterImageView: UIImageView!
     
     @IBOutlet weak var movieTitleLabel: UILabel!
@@ -22,7 +25,22 @@ class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var movieDescriptionTextView: UITextView!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        setupLayout()
+        
+        // Tests
+        movieTitleLabel.text = "This Is The Favourite Movie!"
+        releaseYearLabel.text = "2017"
+        ratingLabel.text = "9.5"
+        
+    }
+    
+    // Constraints setting function
+    //
+    func setupLayout() {
+        moviePosterImageView.layer.cornerRadius = moviePosterImageViewCornerRadius
+        moviePosterImageView.clipsToBounds = true
     }
     
     @IBAction func tapGestureAction(_ sender: UITapGestureRecognizer) {
