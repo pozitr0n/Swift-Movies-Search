e7d19a067d1001e64d15b8ffa0ee2b0fb30baa14
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-09-06 02:04:53 +0200

    Fixed pod file: source= added and some other bugs for release

--- Pods/SwiftEntryKit/Source/Extensions/UIApplication+EKAppearance.swift
+++ Pods/SwiftEntryKit/Source/Extensions/UIApplication+EKAppearance.swift
@@ -12,7 +12,5 @@ extension UIApplication {
     
     func set(statusBarStyle: EKAttributes.StatusBar) {
         let appearance = statusBarStyle.appearance
-        UIApplication.shared.isStatusBarHidden = !appearance.visible
-        UIApplication.shared.statusBarStyle = appearance.style
     }
 }
